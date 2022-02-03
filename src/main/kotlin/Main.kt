6f9f11baf50f6cf5867d09be5667506f11dbade2
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-03 16:40:27 +0000

    Player levels on else if loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,29 @@
 const val HERO_NAME = "Madrigal"
 
 fun main() {
-    var hasSteed = false
-    val pubName = "The Unicorn's Horn"
-    var publicansName = ""
-    var playerGoldAmount = 50
     println("The hero announces her presence to the world.")
+
     println(HERO_NAME)
-    println(HERO_NAME.reversed())
     var playerLevel = 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    if (playerLevel == 1) {
+        println("Meet Mr Bubbles in the land of soft things.")
+    } else if (playerLevel <= 5) {
+        println("Save the town from barbarian invasions.")
+    } else if (playerLevel == 6) {
+        println("Locate the enchanted sword.")
+    } else if (playerLevel == 7){
+        println("Recover the long-lost artifact of creation.")
+    } else if (playerLevel == 8) {
+        println("Defeat Nogartse, bringer of death and eater of worlds.")
+    } else {
+        println("There are no quests right now")
+    }
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel += 1
     println(playerLevel)
 
