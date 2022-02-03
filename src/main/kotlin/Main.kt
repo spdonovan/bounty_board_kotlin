26ea2756125580337a42b1cbdb9f82d5f0d3ce67
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-03 16:24:20 +0000

    first challenges completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,19 @@
+const val HERO_NAME = "Madrigal"
+
 fun main() {
-    println("Hello, world!")
-    var playerLevel: Int = 4
+    var hasSteed = false
+    val pubName = "The Unicorn's Horn"
+    var publicansName = ""
+    var playerGoldAmount = 50
+    println("The hero announces her presence to the world.")
+    println(HERO_NAME)
+    println(HERO_NAME.reversed())
+    var playerLevel = 4
     println(playerLevel)
 
     println("The hero embarks on her journey to locate the enchanted sword.")
-    playerLevel = 5
+    playerLevel += 1
     println(playerLevel)
+
+
 }
