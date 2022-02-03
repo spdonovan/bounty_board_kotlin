081bd067bf4804bf594837f5299af4b789a408ef
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-03 17:20:32 +0000

    refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,27 +8,30 @@ fun main() {
     val hasBefriendedBarbarians = true
     val hasAngeredBarbarians = false
     val playerClass = "paladin"
-    println(playerLevel)
 
-    if (playerLevel == 1) {
-        println("Meet Mr Bubbles in the land of soft things.")
+    val quest: String = if (playerLevel == 1) {
+        "Meet Mr Bubbles in the land of soft things."
     } else if (playerLevel <= 5) {
-        val canTalkToBarbarians = !hasAngeredBarbarians &&(hasBefriendedBarbarians || playerClass == "barbarian")
-        val barbarianQuest: String = if (canTalkToBarbarians) {
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+
+        if (canTalkToBarbarians) {
             "Convince the barbarians to call off their invasion."
         } else {
             "Save the town from barbarian invasions."
         }
-        println(barbarianQuest)
     } else if (playerLevel == 6) {
-        println("Locate the enchanted sword.")
+        "Locate the enchanted sword."
     } else if (playerLevel == 7){
-        println("Recover the long-lost artifact of creation.")
+        "Recover the long-lost artifact of creation."
     } else if (playerLevel == 8) {
-        println("Defeat Nogartse, bringer of death and eater of worlds.")
+        "Defeat Nogartse, bringer of death and eater of worlds."
     } else {
-        println("There are no quests right now")
+        "There are no quests right now"
     }
+
+        println("The hero approaches the bounty board. It reads:")
+        println(quest)
 
     println("Time passes...")
     println("The hero returns from her quest.")
