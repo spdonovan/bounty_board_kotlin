d1377043d31ac15b249c470754596696896c70a5
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-03 14:29:53 +0000

    main function created

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("Hello, world!")
+    var playerLevel: Int = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel = 5
+    println(playerLevel)
+}
