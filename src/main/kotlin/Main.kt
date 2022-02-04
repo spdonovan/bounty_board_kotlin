959a0484659989fa09894fcc80f366497845a1f3
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-04 17:23:15 +0000

    split into seperate functions and refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,55 @@
 const val HERO_NAME = "Madrigal"
+var playerLevel = 0
 
 fun main() {
-    println("The hero announces her presence to the world.")
-
-    println(HERO_NAME)
-    var playerLevel = 4
-    val hasBefriendedBarbarians = true
-    val hasAngeredBarbarians = false
-    val playerClass = "paladin"
-
-    val quest: String = when (playerLevel) {
-         1 -> { "Meet Mr Bubbles in the land of soft things." }
-         in 2..5 -> {
-            val canTalkToBarbarians = !hasAngeredBarbarians &&
-                    (hasBefriendedBarbarians || playerClass == "barbarian")
-            if (canTalkToBarbarians) {
-                "Convince the barbarians to call off their invasion."
-            } else { "Save the town from barbarian invasions." }
-        }
-        6 -> "Locate the enchanted sword."
-        7 -> "Recover the long-lost artifact of creation."
-        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
-        else ->  "There are no quests right now"
+    println("$HERO_NAME announces her presence to the world.")
+    println("Which level is $HERO_NAME?")
+    val playerLevelInput = readLine()!!
+    playerLevel = if (playerLevelInput.matches("""\d+""".toRegex())) {
+        playerLevelInput.toInt()
+    } else {
+        1
     }
+    println("$HERO_NAME's level is $playerLevel.")
 
-        println("The hero approaches the bounty board. It reads:")
-                println (quest)
+    readBountyBoard()
 
                 println ("Time passes...")
-                println ("The hero returns from her quest.")
+                println ("$HERO_NAME returns from her quest.")
 
                 playerLevel += 1
             println(playerLevel)
+    readBountyBoard()
+}
 
+private fun readBountyBoard() {
+    println(
+        """
+        $HERO_NAME approaches the bounty board. It reads:
+        "${obtainQuest(playerLevel).replace("Nogartse", "xxxxxxxx")}"
+           """.trimIndent())
+}
 
+private fun obtainQuest(
+    playerLevel: Int,
+    playerClass: String = "paladin",
+    hasBefriendedBarbarians: Boolean = true,
+    hasAngeredBarbarians: Boolean = false
+): String = when (playerLevel) {
+    1 -> {
+        "Meet Mr Bubbles in the land of soft things."
+    }
+    in 2..5 -> {
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from barbarian invasions."
+        }
+    }
+    6 -> "Locate the enchanted sword."
+    7 -> "Recover the long-lost artifact of creation."
+    8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+    else -> "There are no quests right now"
 }
