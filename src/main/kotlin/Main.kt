2bcf2c34cd7c0ad122e71c73e38d704e2bf7fffc
ScottDonovan <spdonovan@hotmail.co.uk>
2022-02-04 14:02:35 +0000

    refactor using when statement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,35 +9,29 @@ fun main() {
     val hasAngeredBarbarians = false
     val playerClass = "paladin"
 
-    val quest: String = if (playerLevel == 1) {
-        "Meet Mr Bubbles in the land of soft things."
-    } else if (playerLevel <= 5) {
-        val canTalkToBarbarians = !hasAngeredBarbarians &&
-                (hasBefriendedBarbarians || playerClass == "barbarian")
-
-        if (canTalkToBarbarians) {
-            "Convince the barbarians to call off their invasion."
-        } else {
-            "Save the town from barbarian invasions."
+    val quest: String = when (playerLevel) {
+         1 -> { "Meet Mr Bubbles in the land of soft things." }
+         in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians &&
+                    (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else { "Save the town from barbarian invasions." }
         }
-    } else if (playerLevel == 6) {
-        "Locate the enchanted sword."
-    } else if (playerLevel == 7){
-        "Recover the long-lost artifact of creation."
-    } else if (playerLevel == 8) {
-        "Defeat Nogartse, bringer of death and eater of worlds."
-    } else {
-        "There are no quests right now"
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds."
+        else ->  "There are no quests right now"
     }
 
         println("The hero approaches the bounty board. It reads:")
-        println(quest)
+                println (quest)
 
-    println("Time passes...")
-    println("The hero returns from her quest.")
+                println ("Time passes...")
+                println ("The hero returns from her quest.")
 
-    playerLevel += 1
-    println(playerLevel)
+                playerLevel += 1
+            println(playerLevel)
 
 
 }
